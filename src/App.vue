ebdaf5b9f96f42929675bd3ef52187ac74b62704
IrisTc <1010939908@qq.com>
2021-04-09 11:46:13 +0800

    添加标题

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <i class="index-title-left">
         <img src="./assets/title-left.png" alt="tag-left" />
       </i>
+      <div id="title" :style="{ height: '100%', width: '100%' }"></div>
       <i class="index-title-right">
         <img src="./assets/title-right.png" alt="tag-right" />
       </i>
@@ -14,12 +15,31 @@
 </template>
 
 <script>
-import Map from "@/views/map.vue"
+import Map from "@/views/map.vue";
 
 export default {
   name: "App",
   components: {
     Map,
+  },
+  data() {
+    return {
+      titleOption: {
+        title: {
+          text: "浙江省博士后实时数据运行平台",
+          subtext: "",
+          left: "center",
+          textStyle: {
+            color: "#FFAC50",
+            fontSize: 40,
+          },
+        },
+      },
+    };
+  },
+  mounted() {
+    let myChart = this.$echarts.init(document.getElementById("title"));
+    myChart.setOption(this.titleOption);
   },
 };
 </script>
